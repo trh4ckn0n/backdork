63a3f417c9fda2c02ce0415d21750f43042119cc
trh4ckn0n <trhacknon@proton.me>
2025-06-23 00:43:17 +0200

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DorkHunter from './App';
+import './index.css'; // optionnel, si tu veux des styles globaux
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <DorkHunter />
+  </React.StrictMode>
+);
