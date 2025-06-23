e241136b1b9908056c1fa06ea23afd26fa5eaea9
trh4ckn0n <trhacknon@proton.me>
2025-06-23 02:12:26 +0200

    Update index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DorkHunter from './app';
+import DorkHunter from './App.jsx';
 import './index.css'; // optionnel, si tu veux des styles globaux
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
